f53f387f819bf777308e3b8a82181b1a2e728fa6
Junior Romero <jumiroma007@gmail.com>
2024-05-26 18:06:54 -0500

    Manejo de estados y props

--- hola-mundo/src/App.jsx
+++ hola-mundo/src/App.jsx
@@ -1,15 +1,49 @@
+import {useState} from 'react';
 import './App.css'
 import { TwitterFollowCard } from './twitterFollowCard'
 //le estamos diciendo al empaquetador de aplicaciones que en este archivo voy a usar .jsx
 
 export function App(){
     //<> usar esto en vez de React.Fragment
+    const fFormat = (username) => `@${username}`;
+    //por propieda también se puede enviar elementos como <span>@xxx</span>
+    const [name,setName]=useState ('imadorisempai');
+
+    console.log('render with name: ', name);
+    const changeName = () =>{
+        setName('midudev'); //los cambios se propagan a los niveles inferiores por ello vuelve renderizar todos los twitterfollowcard
+    };
+
+    //imadori es un objeto y se puede pasar todo un objeto y pasar todas las propiedades juntas 
+    const imadori = {formatUserName:fFormat,username:name, initialIsFollowing:true};
+ 
     return (
-        <div className='App'> 
-            <TwitterFollowCard username="imadorisempai" name="Junior Miguel Romero Maza" isFollowing/>
-            <TwitterFollowCard username="pamelaanderson" name="Pamela Anderson" isFollowing/>
-            <TwitterFollowCard username="SergioRamos" name="Sergio Ramos" isFollowing={false}/>
-            <TwitterFollowCard username="KMbappe" name="Kylian Mbappé" isFollowing/>
-        </div>
+        //los nombres que aparecen son children de twitterfollowcarc
+        <section className='App'> 
+        {/* comentario dentro del render */}
+            <TwitterFollowCard {...imadori}>
+                    Junior Miguel Romero Maza
+            </TwitterFollowCard> 
+            <TwitterFollowCard
+                formatUserName={fFormat} 
+                username="pamelaanderson" >
+                    Pamela Anderson
+            </TwitterFollowCard> 
+            <TwitterFollowCard
+                formatUserName={fFormat} 
+                username="SergioRamos">
+                    Sergio Ramos
+            </TwitterFollowCard> 
+            <TwitterFollowCard
+                formatUserName={fFormat} 
+                username="KMbappe" 
+                initialIsFollowing={true}>
+                    Kylian Mbappé
+            </TwitterFollowCard> 
+            <TwitterFollowCard
+                formatUserName={fFormat}>
+            </TwitterFollowCard> 
+            <button onClick={changeName}>Cambio nombre </button>
+        </section>
     )
 }
--- hola-mundo/src/twitterFollowCard.jsx
+++ hola-mundo/src/twitterFollowCard.jsx
@@ -1,22 +1,39 @@
-import './App.css'
+import {useState} from 'react'; //manejar estados hooks permiten añadir funcionaliddad a los componentes, diferentes puntos del renderizado
+import './App.css';
 //le estamos diciendo al empaquetador de aplicaciones que en este archivo voy a usar .jsx
 //la base de la reutilización en react son los parámetros las propiedades
 //para que un componente sea reutilizable se tiene que pasarle parámetros
-export function TwitterFollowCard({username, name,isFollowing}){
+//lo que está entre llaves se llaman propiedades: props y deberían ser inmutables
+export function TwitterFollowCard({children, formatUserName,username='unknown', initialIsFollowing}){
+    // username = `@${username}` esto es un error porque se está modificando una prop mejor crear constantes
     const imageSrc= `https://unavatar.io/twitter/${username}`; //crear constante y un template string
-    console.log({isFollowing});
+
+    // también se puede recibir elementos como parámetros
+    //children en este ejemplo es un nombre pero podría ser un elemento o hasta un componente
+    //el botón seguir enseñará el manejo de estados en react pues el botón tendrá un comportamiento si seguimos o no a una persona
+    //si se una prop para inicializar un estado, esto solo se inicializa una vez
+    const [isFollowing,setIsFollowing]=useState(initialIsFollowing); //devuelve un array primera posicion valor del estado y la segunda funcion que permite actualizar ese estado
+    
+    const text = isFollowing?  'Siguiendo' :'Seguir'; //ternaria
+    const buttonClass=  isFollowing? 'tw-follow-card-button' :'tw-follow-card-button is-following';
+    
+    const handleClick = () => {
+        setIsFollowing(!isFollowing);
+    };
+
+    console.log("Twitter follow card, username: ",username);
     return (
         <article className="tw-follow-card">
             <header className="tw-follow-card-header">
                 <img alt="Avatar DevianArt" src={imageSrc}/>
                 <div>
-                    <strong>{name}</strong>
-                    <span>@{username}</span>
+                    <strong>{children}</strong>
+                    <span>{formatUserName(username)}</span>
                 </div>
             </header>
             <aside>
-                <button>
-                    Seguir
+                <button className={buttonClass} onClick={handleClick}>
+                    {text}  
                 </button>
             </aside>
         </article>
